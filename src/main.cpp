fb4fd46b21564c98060e7dbb081bd4e89ada5a3e
kdesnos <kdesnos@gmail.com>
2016-01-30 14:45:03 +0000

    (Algo) Multi pattern detection.
    git-svn-id: https://svn.code.sf.net/p/arboardgame/code@8 af02ebe8-ddd1-4f06-940f-05ec0f1e6110

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,98 @@
 using namespace cv;
 using namespace cv::xfeatures2d;
 
+
+#define NB_IMAGE_PATHS 2
+String image_paths[][2] = {
+     {"Cat","D:/ARBoardGame/data/cat.png"},
+    {"Bite","D:/ARBoardGame/data/bite.png"}
+
+};
+
+typedef struct Image {
+    String name;
+    Mat img;
+    Mat descriptors_object;
+    std::vector<KeyPoint> keypoints_object;
+} Image;
+
+void detectObject(String name, Mat& frame, Mat& img_scene, Mat& descriptors_scene, std::vector<KeyPoint>& keypoints_scene, Mat& img_object ,Mat& descriptors_object, std::vector<KeyPoint>& keypoints_object){
+    //-- Step 2: Matching descriptor vectors using FLANN matcher
+    FlannBasedMatcher matcher;
+    std::vector< DMatch > matches;
+
+    matcher.match( descriptors_object, descriptors_scene, matches );
+
+    double max_dist = 0; double min_dist = 100;
+
+    //-- Quick calculation of max and min distances between keypoints
+    for( int i = 0; i < descriptors_object.rows; i++ )
+    { double dist = matches[i].distance;
+        if( dist < min_dist ) min_dist = dist;
+        if( dist > max_dist ) max_dist = dist;
+    }
+
+    //-- Localize the object
+    std::vector< DMatch > good_matches;
+    for( int i = 0; i < descriptors_object.rows; i++ )
+    { if( matches[i].distance < 4*min_dist )
+        { good_matches.push_back( matches[i]); }
+    }
+
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
+
+    for( size_t i = 0; i < good_matches.size(); i++ )
+    {
+        //-- Get the keypoints from the good matches
+        obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+        scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+    }
+
+    Mat Hinv = findHomography( scene, obj, RANSAC );
+    Mat img_corrected;
+    warpPerspective(img_scene,img_corrected,Hinv,Size(img_object.cols, img_object.rows));
+
+    imshow(name, img_corrected);
+
+    Mat isDetected;
+    matchTemplate(img_corrected, img_object, isDetected,TM_CCOEFF_NORMED);
+
+    String detected = (isDetected.at<float>(0,0) > 0.4)?"ok":"nope";
+    std::cout << name << " - " << detected << " " << isDetected.at<float>(0,0) << std::endl;
+
+    // Update main frame
+#define DISPLAY
+#ifdef DISPLAY
+    Mat H = findHomography( obj, scene, RANSAC );
+
+    //-- Get the corners from the image_1 ( the object to be "detected" )
+    std::vector<Point2f> obj_corners(4);
+    obj_corners[0] = cvPoint(0,0);
+    obj_corners[1] = cvPoint( img_object.cols, 0 );
+    obj_corners[2] = cvPoint( img_object.cols, img_object.rows );
+    obj_corners[3] = cvPoint( 0, img_object.rows );
+    std::vector<Point2f> scene_corners(4);
+
+
+
+    perspectiveTransform( obj_corners, scene_corners, H);
+
+
+    // Display points in the frame
+    for(int i =0; i< scene.size(); i++){
+       circle(frame, scene[i], 10,  Scalar(128, 128, 128));
+    }
+
+    Scalar color = (isDetected.at<float>(0,0) > 0.4)? Scalar(0, 255, 0): Scalar(0, 0, 255);
+    line( frame, scene_corners[0] , scene_corners[1], color, 4 );
+    line( frame, scene_corners[1], scene_corners[2], color, 4 );
+    line( frame, scene_corners[2], scene_corners[3], color, 4 );
+    line( frame, scene_corners[3], scene_corners[0], color, 4 );
+
+#endif
+}
+
 int main(int, char**)
 {
     VideoCapture cap(0); // open the default camera
@@ -22,19 +114,27 @@ int main(int, char**)
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
-    // Reference picture
-    Mat img_object = imread( "D:/ARBoardGame/data/CarteToDetect2.png", IMREAD_GRAYSCALE );
-
-    // Detect the keypoints and extract descriptors using SURF
     int minHessian = 400;
     Ptr<SURF> detector = SURF::create( minHessian );
-    std::vector<KeyPoint> keypoints_object;
-    Mat descriptors_object;
+    std::vector<Image> images;
+    for(int i =0; i< NB_IMAGE_PATHS; i++){
+        Image im;
 
-    detector->detectAndCompute( img_object, Mat(), keypoints_object, descriptors_object );
+        // Reference picture
+        im.name = image_paths[i][0];
+        im.img = imread( image_paths[i][1], IMREAD_GRAYSCALE );
 
-    if( !img_object.data )
-    { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+        if( !im.img.data )
+        {
+            std::cout<< " --(!) Error reading image: " << image_paths[i] << std::endl;
+            continue;
+        }
+
+        // Detect the keypoints and extract descriptors using SURF
+        detector->detectAndCompute( im.img, Mat(), im.keypoints_object, im.descriptors_object );
+        images.push_back(im);
+    }
+
 
     namedWindow("edges",1);
     for(;;)
@@ -48,92 +148,16 @@ int main(int, char**)
         Mat descriptors_scene;
         detector->detectAndCompute( img_scene, Mat(), keypoints_scene, descriptors_scene );
 
-        //-- Step 2: Matching descriptor vectors using FLANN matcher
-        FlannBasedMatcher matcher;
-        std::vector< DMatch > matches;
-        try {
-            matcher.match( descriptors_object, descriptors_scene, matches );
-        } catch(...) {
-            continue;
-        }
-        double max_dist = 0; double min_dist = 100;
-
-
-
-        //-- Quick calculation of max and min distances between keypoints
-        for( int i = 0; i < descriptors_object.rows; i++ )
-        { double dist = matches[i].distance;
-            if( dist < min_dist ) min_dist = dist;
-            if( dist > max_dist ) max_dist = dist;
+        for(int i = 0; i< images.size();i++){
+            try{
+                detectObject(images.at(i).name, frame, img_scene, descriptors_scene, keypoints_scene, images.at(i).img, images.at(i).descriptors_object, images.at(i).keypoints_object);
+            } catch(...){
+                continue;
+            }
         }
 
-        //-- Localize the object
-        std::vector< DMatch > good_matches;
-        for( int i = 0; i < descriptors_object.rows; i++ )
-        { if( matches[i].distance < 4*min_dist )
-            { good_matches.push_back( matches[i]); }
-        }
-
-        Mat img_matches;
-
-        std::vector<Point2f> obj;
-        std::vector<Point2f> scene;
-
-        for( size_t i = 0; i < good_matches.size(); i++ )
-        {
-            //-- Get the keypoints from the good matches
-            obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
-            scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
-        }
-
-        Mat Hinv = findHomography( scene, obj, RANSAC );
-
-        Mat img_corrected;
-        try {
-            warpPerspective(img_scene,img_corrected,Hinv,Size(img_object.cols, img_object.rows));
-        } catch(...) {
-            continue;
-        }
-
-        imshow("Detection", img_corrected);
-
-        Mat test;
-        matchTemplate(img_corrected, img_object, test,TM_CCOEFF_NORMED);
-
-        String detected = (test.at<float>(0,0) > 0.5)?"ok":"nope";
-        std::cout << test.at<float>(0,0) << " - " << detected << std::endl;
-
- #define AFFICHE
-#ifdef AFFICHE
-
-        Mat H = findHomography( obj, scene, RANSAC );
-
-        //-- Get the corners from the image_1 ( the object to be "detected" )
-        std::vector<Point2f> obj_corners(4);
-        obj_corners[0] = cvPoint(0,0);
-        obj_corners[1] = cvPoint( img_object.cols, 0 );
-        obj_corners[2] = cvPoint( img_object.cols, img_object.rows );
-        obj_corners[3] = cvPoint( 0, img_object.rows );
-        std::vector<Point2f> scene_corners(4);
-
-
-        try {
-            perspectiveTransform( obj_corners, scene_corners, H);
-        } catch(...) {
-            continue;
-        }
-
-        for(int i =0; i< scene.size(); i++){
-            circle(img_scene, scene[i], 10,  Scalar(0, 255, 0));
-        }
-
-        line( img_scene, scene_corners[0] , scene_corners[1], Scalar(0, 255, 0), 4 );
-        line( img_scene, scene_corners[1], scene_corners[2], Scalar( 0, 255, 0), 4 );
-        line( img_scene, scene_corners[2], scene_corners[3], Scalar( 0, 255, 0), 4 );
-        line( img_scene, scene_corners[3], scene_corners[0], Scalar( 0, 255, 0), 4 );
-
-        imshow("edges", img_scene);
-#endif
+        // display frame with detected objects
+        imshow("edges", frame);
 
 
         if(waitKey(30) >= 0) break;
