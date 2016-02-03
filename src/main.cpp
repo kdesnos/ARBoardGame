9662dc006abbb8113df35ab52599634608eb2872
FrancoisChapeau <7n7.1337@gmail.com>
2016-02-03 19:03:02 +0100

    (Engine) Slots shoot ennemies.
    git-svn-id: https://svn.code.sf.net/p/arboardgame/code@11 af02ebe8-ddd1-4f06-940f-05ec0f1e6110

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,9 @@
 #include "opencv2/opencv.hpp"
 #include <windows.h>
 
-//#define DISPLAY
+#define DISPLAY
+//#define DISPLAY_CAM
+//#define DISPLAY_SMALL
 #define THRESHOLD 0.4
 #define STEP 0.2
 #define SCREENWIDTH 900
@@ -36,6 +38,23 @@ String image_paths[][2] = {
     {"Mine",    ROOTPATH "landmine.png"}
 };
 
+
+
+// TOOLS
+
+float getRand()
+{
+    return (rand()%1000)/1000.0;
+}
+
+float getRandAngle(){
+    return -M_PI + getRand()*M_PI*2.0;
+}
+
+
+
+// STRUCTS
+
 typedef struct Image {
     String name;
     Mat img;
@@ -48,23 +67,27 @@ typedef struct Image {
 typedef struct Entity {
     String name;
     Mat img;
-    float x;
-    float y;
-    float hp;
-    float speed;
-    Entity(String _name){ name = _name; x = 0.0; y = 0.0; hp = 100.0; speed = 0.03; }
+    float w;
+    float r;
+    Entity(String _name){ name = _name; w = 0.0; r = 0.1; }
 } Entity;
 
+typedef struct Ennemy {
+    String name;
+    Mat img;
+    float r;
+    float w;
+    float hp;
+    float dr;
+    float dw;
+    Ennemy(String _name){ name = _name; r = 0.9; w = getRandAngle(); hp = 50.0 + 100.0*getRand(); dr = 0.05+0.03*getRand(); dw = 0.0; }
+} Ennemy;
+
 typedef struct Weapon {
-    int slot;
-    float built;
-    float dmgAir;
-    float dmgMagic;
-    float dmgGround;
     float dps;
     float angle;
     float range;
-    Weapon(){ slot = 0; built = 0.0; dmgAir = 0.33; dmgMagic = 0.33; dmgGround = 0.33; dps = 1.0; angle = M_PI/2; range = 0.5;}
+    Weapon(){ dps = 30.0; angle = getRandAngle()*0.5; range = 0.3 + 0.5 * getRand(); }
 } Weapon;
 
 
@@ -73,102 +96,193 @@ typedef struct Weapon {
 
 int startTime = GetTickCount();
 int lastTime = startTime;
+std::vector<Image> images;
 std::vector<Entity> ents;
-std::vector<Weapon> weaps(NB_WEAPONS);
+std::vector<Ennemy> ennemies;
+std::vector<Weapon> weaps;
+Mat frame;
+Mat display(SCREENHEIGHT, SCREENWIDTH, CV_8UC3);
 
 
 
-float getRand()
+// METHODS
+
+int getSlot(float x, float y)
 {
-    return (rand()%1000)/1000.0;
+    int slot = (y / frame.rows) > 0.5 ? 3 : 0;
+    slot += (x / frame.cols) > 0.66 ? 3 : ((x / frame.cols) > 0.33 ? 2 : 1);
+    int lut[] = {0, 6, 5, 4, 1, 2, 3};
+    return lut[slot];
 }
 
 void populateGame()
 {
     // VIRGIN
     ents.push_back(Entity("virgin"));
-    ents.back().x = 0.0;
-    ents.back().y = 0.0;
+    ents.back().w = 0.0;
+    ents.back().r = 0.0;
     ents.back().img = imread( ROOTPATH "virgin.png", IMREAD_COLOR );
 
     // SLOTS
     float radius = 0.15;
     float angle = M_PI/6.0;
     ents.push_back(Entity("slot1"));
-    ents.back().x = POL_TO_X(angle, radius);
-    ents.back().y = POL_TO_Y(angle, radius);
+    ents.back().w = angle;
+    ents.back().r = radius;
     ents.back().img = imread( ROOTPATH "slot1.png", IMREAD_COLOR );
     angle += M_PI/3.0;
     ents.push_back(Entity("slot1"));
-    ents.back().x = POL_TO_X(angle, radius);
-    ents.back().y = POL_TO_Y(angle, radius);
+    ents.back().w = angle;
+    ents.back().r = radius;
     ents.back().img = imread( ROOTPATH "slot2.png", IMREAD_COLOR );
     angle += M_PI/3.0;
     ents.push_back(Entity("slot1"));
-    ents.back().x = POL_TO_X(angle, radius);
-    ents.back().y = POL_TO_Y(angle, radius);
+    ents.back().w = angle;
+    ents.back().r = radius;
     ents.back().img = imread( ROOTPATH "slot3.png", IMREAD_COLOR );
     angle += M_PI/3.0;
     ents.push_back(Entity("slot1"));
-    ents.back().x = POL_TO_X(angle, radius);
-    ents.back().y = POL_TO_Y(angle, radius);
+    ents.back().w = angle;
+    ents.back().r = radius;
     ents.back().img = imread( ROOTPATH "slot4.png", IMREAD_COLOR );
     angle += M_PI/3.0;
     ents.push_back(Entity("slot1"));
-    ents.back().x = POL_TO_X(angle, radius);
-    ents.back().y = POL_TO_Y(angle, radius);
+    ents.back().w = angle;
+    ents.back().r = radius;
     ents.back().img = imread( ROOTPATH "slot5.png", IMREAD_COLOR );
     angle += M_PI/3.0;
     ents.push_back(Entity("slot1"));
-    ents.back().x = POL_TO_X(angle, radius);
-    ents.back().y = POL_TO_Y(angle, radius);
+    ents.back().w = angle;
+    ents.back().r = radius;
     ents.back().img = imread( ROOTPATH "slot6.png", IMREAD_COLOR );
+
+    // WEAPONS
+    for(unsigned int i=0; i<NB_WEAPONS; i++){
+        weaps.push_back(Weapon());
+    }
+
+    assert(weaps.size() == NB_WEAPONS);
 
 }
 
 void spawnEnnemies(float totalTime)
 {
     if(getRand()<0.1){
-        float radius = 0.9;
-        float angle = -M_PI + getRand()*M_PI*2.0;
-        ents.push_back(Entity("roucoups"));
-        ents.back().x = POL_TO_X(angle, radius);
-        ents.back().y = POL_TO_Y(angle, radius);
-        ents.back().img = imread( ROOTPATH "roucoups.png", IMREAD_COLOR );
+        ennemies.push_back(Ennemy("roucoups"));
+        ennemies.back().img = imread( ROOTPATH "roucoups.png", IMREAD_COLOR );
     }
 }
 
-void drawImage(Mat &src, Mat &dst, float x, float y)
+void drawImage(Mat &src, float x, float y)
 {
     int px = ( x / 2.0 + 0.5) * SCREENWIDTH  - src.cols / 2.0;
     int py = (-y / 2.0 + 0.5) * SCREENHEIGHT - src.rows / 2.0;
-    src.copyTo(dst(Rect(px, py, src.cols, src.rows)));
+    src.copyTo(display(Rect(px, py, src.cols, src.rows)));
 }
 
-void drawEntities(Mat &dst)
+void drawLine(float x1, float y1, float x2, float y2, const Scalar& bgr){
+    float px1 = ( x1 / 2.0 + 0.5) * SCREENWIDTH;
+    float py1 = (-y1 / 2.0 + 0.5) * SCREENHEIGHT;
+    float px2 = ( x2 / 2.0 + 0.5) * SCREENWIDTH;
+    float py2 = (-y2 / 2.0 + 0.5) * SCREENHEIGHT;
+    line(display, Point(px1, py1), Point(px2, py2), bgr);
+}
+
+void drawWorld()
 {
     for(unsigned int i=0; i<ents.size(); i++){
-        drawImage(ents.at(i).img, dst, ents.at(i).x, ents.at(i).y);
+        float w = ents.at(i).w;
+        float r = ents.at(i).r;
+        drawImage(ents.at(i).img, POL_TO_X(w,r), POL_TO_Y(w,r));
+    }
+    for(unsigned int i=0; i<ennemies.size(); i++){
+        if(ennemies.at(i).hp > 0.0){
+            float w = ennemies.at(i).w;
+            float r = ennemies.at(i).r;
+            float x = POL_TO_X(w,r);
+            float y = POL_TO_Y(w,r);
+            float hp = ennemies.at(i).hp;
+            drawImage(ennemies.at(i).img, x, y);
+            float dy = 1.1*ennemies.at(i).img.rows/SCREENHEIGHT;
+            float dx = hp/100.0*ennemies.at(i).img.cols/SCREENWIDTH;
+            drawLine(x-dx, y+dy, x+dx, y+dy, Scalar(0,0,255));
+        }
     }
 }
 
 void moveEnemies(float deltaTime)
 {
-    for(unsigned int i=7; i<ents.size(); i++){
-        float x = ents.at(i).x;
-        float y = ents.at(i).y;
-        float n = sqrt(x*x+y*y);
-        float newn = n - ents.at(i).speed*deltaTime;
-        ents.at(i).x = x * (newn/n);
-        ents.at(i).y = y * (newn/n);
+    for(unsigned int i=0; i<ennemies.size(); i++){
+        if(ennemies.at(i).hp > 0.0){
+            ennemies.at(i).r = ennemies.at(i).r - ennemies.at(i).dr * deltaTime;
+            ennemies.at(i).w = ennemies.at(i).w + ennemies.at(i).dw * deltaTime;
+        }
     }
 }
 
+void handleWeapons()
+{
+    for(unsigned int i=0; i<NB_WEAPONS; i++){
+        if(images.at(i).detected > 0.5){
+            int slot = getSlot(images.at(i).position.x, images.at(i).position.y);
+            float w = ents.at(slot).w;
+            float r = ents.at(slot).r;
+            float angle = weaps.at(i).angle;
+            float range = weaps.at(i).range;
 
+            // Draw cone
+            float ang1 = w+angle/2;
+            float ang2 = w-angle/2;
+            float x0 = r * cos(w);
+            float y0 = r * sin(w);
+            float x1 = x0 + range * cos(ang1);
+            float y1 = y0 + range * sin(ang1);
+            drawLine(x0,y0,x1,y1, Scalar(255,0,0));
+            float x2 = x0 + range * cos(ang2);
+            float y2 = y0 + range * sin(ang2);
+            drawLine(x0,y0,x2,y2, Scalar(255,0,0));
+
+            // Find nearest ennemy
+            Ennemy* nearest = NULL;
+            for(unsigned int j=0; j<ennemies.size(); j++){
+                if(ennemies.at(j).hp > 0.0){
+                    float dw = ennemies.at(j).w - w;
+                    while(dw <= -M_PI){ dw += 2*M_PI; }
+                    while(dw > M_PI){ dw -= 2*M_PI; }
+                    if(-angle/2 <= dw && dw <= angle/2){ // In cone
+                        if(!nearest || ennemies.at(j).r < nearest->r){
+                            nearest = &ennemies.at(j);
+                        }
+                    }
+                }
+            }
+
+            // Hit nearest
+            if(nearest && nearest->r < range){
+                nearest->hp -= weaps.at(i).dps;
+                float x = POL_TO_X(nearest->w, nearest->r);
+                float y = POL_TO_Y(nearest->w, nearest->r);
+                drawLine(x0,y0,x,y, Scalar(255,255,0));
+            }
+
+        }
+    }
+}
+
+void handleEndGame(){
+    for(unsigned int i=0; i<ennemies.size(); i++){
+        if(ennemies.at(i).r < 0.0){
+            Mat endgame = imread( ROOTPATH "endgame.png", IMREAD_COLOR );
+            imshow("GAME OVER", endgame);
+            for(;;){ if(waitKey(30) >= 0) break; }
+            return;
+        }
+    }
+}
 
 // GAMEPLAY
 
-void gameplay(std::vector<Image> &images)
+void gameplay()
 {
     int thisTime = GetTickCount();
     float deltaTime = (thisTime-lastTime)/1000.0;
@@ -180,16 +294,31 @@ void gameplay(std::vector<Image> &images)
     std::cout << "deltaTime: " << deltaTime << " s\n" << std::endl;
 
     // Create green background
-    Mat display(SCREENHEIGHT, SCREENWIDTH, CV_8UC3);
     display = cv::Scalar(100,200,0);
 
+    handleWeapons();
     spawnEnnemies(totalTime);
     moveEnemies(deltaTime);
-    drawEntities(display);
+    handleEndGame();
+    drawWorld();
 
     imshow("display", display);
     lastTime = thisTime;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -271,7 +400,9 @@ void detectObject(String name, Mat& frame, Mat& img_scene, Mat& descriptors_scen
     line( img_corrected_colored, obj_corners[1], obj_corners[2], color, 4 );
     line( img_corrected_colored, obj_corners[2], obj_corners[3], color, 4 );
     line( img_corrected_colored, obj_corners[3], obj_corners[0], color, 4 );
+#ifdef DISPLAY_SMALL
     imshow(name, img_corrected_colored);
+#endif
 
     // Update main frame
     // Display points in the frame
@@ -308,7 +439,6 @@ int main(int, char**)
 
     int minHessian = 400;
     Ptr<SURF> detector = SURF::create( minHessian );
-    std::vector<Image> images;
     for(int i =0; i< NB_WEAPONS; i++){
         Image im;
 
@@ -330,7 +460,7 @@ int main(int, char**)
 
     for(;;)
     {
-        Mat frame, img_scene;
+        Mat img_scene;
         cap >> frame; // get a new frame from camera
         cvtColor(frame, img_scene, COLOR_BGR2GRAY);
 
@@ -346,12 +476,15 @@ int main(int, char**)
                 continue;
             }
         }
-#ifdef DISPLAY
+#ifdef DISPLAY_CAM
         // display frame with detected objects
+        line(frame,Point(0, frame.rows/2), Point(frame.cols, frame.rows/2), Scalar(255,0,0));
+        line(frame,Point(frame.cols/3, 0), Point(frame.cols/3, frame.rows), Scalar(255,0,0));
+        line(frame,Point(2*frame.cols/3, 0), Point(2*frame.cols/3, frame.rows), Scalar(255,0,0));
         imshow("edges", frame);
 #endif
 
-        gameplay(images);
+        gameplay();
 
         if(waitKey(30) >= 0) break;
     }
