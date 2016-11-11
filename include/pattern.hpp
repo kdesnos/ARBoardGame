06e27e103f3a83fbbc7d3990f490bddb3aa7bf56
kdesnos <kdesnos@gmail.com>
2016-11-11 17:08:04 +0100

    (Vision) Add DetectionConfidence to pattern.

--- include/pattern.hpp
+++ include/pattern.hpp
@@ -20,7 +20,6 @@
 */
 class Pattern : public SURFDetectable, virtual NonAssignable
 {
-
 public:
 	/**
 	* Constructor of the Pattern class receiving a path to an image of the 
@@ -39,9 +38,41 @@ public:
 	Pattern(const std::string & name, const std::string & path) throw(...);
 
 	/**
+	* Threshold used to determine whether a detectionConfidence of a pattern 
+	* is positive or negative.
+	*/
+	static const double DETECTION_CONFIDENCE_THRESHOLD ;
+
+	/**
+	* Step used to increase (or decrease) the value of the detectionConfidence.
+	*/
+	static const double DETECTION_CONFIDENCE_STEP;
+
+	/**
 	* Get the _name of the Pattern
 	*/
 	const std::string & getName() const throw() ;
+
+	/**
+	* Get the current _detectionConfidence of the Pattern
+	*
+	* \return double value of the _detectionConfidence
+	*/
+	double getDetectionConfidence() const;
+
+	/**
+	* Increase the current value of the detectionConfidence for the Pattern.
+	*
+	* \return double value of the _detectionConfidence
+	*/
+	double increaseDetectionConfidence();
+
+	/**
+	* Decrease the current value of the detectionConfidence for the Pattern.
+	*
+	* \return double value of the _detectionConfidence
+	*/
+	double decreaseDetectionConfidence();
 
 	/**
 	* Operator used for comparing two patterns. Since no two patterns can have the 
@@ -57,6 +88,21 @@ protected:
 	* (enforced by the constructor).
 	*/
 	const std::string _name;
+
+	/**
+	* Detection confidence for the pattern.
+	*
+	* A detectionConfidence set to 0 means that the pattern is not detected at all.
+	* A detectionConfidence set to 1 means that the pattern is fully detected.
+	* A detectionScore in between means that pattern detection is building (or 
+	* decreasing confidence). 
+	*
+	* Threshold between positive and negative detection is set by the 
+	* Pattern::DETECTION_CONFIDENCE_THRESHOLD constant.
+	*/
+	double _detectionConfidence;
+
+	create position class
 
 private:
 	static std::list<std::reference_wrapper<Pattern &>> _allPatterns;
--- src/pattern.cpp
+++ src/pattern.cpp
@@ -1,8 +1,13 @@
 #include "arExceptions.hpp"
 #include "pattern.hpp"
 
+const double Pattern::DETECTION_CONFIDENCE_STEP = 0.2;
+
+const double Pattern::DETECTION_CONFIDENCE_THRESHOLD = 0.5;
+
 Pattern::Pattern(const std::string & name, const std::string & path) throw(...) :
-	_name(name), SURFDetectable(cv::imread(path, cv::IMREAD_GRAYSCALE))
+	_name(name), SURFDetectable(cv::imread(path, cv::IMREAD_GRAYSCALE)),
+	_detectionConfidence(0.0)
 {
 	// Check whether the image was successfully opened during initialization.
 	if (!_image.data) {
@@ -13,6 +18,25 @@ Pattern::Pattern(const std::string & name, const std::string & path) throw(...) 
 const std::string& Pattern::getName() const throw()
 {
 	return _name;
+}
+
+double Pattern::getDetectionConfidence() const
+{
+	return _detectionConfidence;
+}
+
+double Pattern::decreaseDetectionConfidence()
+{
+	_detectionConfidence -= DETECTION_CONFIDENCE_STEP;
+	_detectionConfidence = (_detectionConfidence < 0.0) ? 0.0 : _detectionConfidence;
+	return _detectionConfidence;
+}
+
+double Pattern::increaseDetectionConfidence()
+{
+	_detectionConfidence += DETECTION_CONFIDENCE_STEP;
+	_detectionConfidence = (_detectionConfidence > 1.0) ? 1.0 : _detectionConfidence;
+	return _detectionConfidence;
 }
 
 bool Pattern::operator==(const Pattern & other) const
